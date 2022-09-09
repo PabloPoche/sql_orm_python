2012c12ae8513a0c5f448a0c993752078ece4564
PabloPoche <107163780+PabloPoche@users.noreply.github.com>
2022-09-08 23:22:14 -0300

    Ejercicio de practica resuelto.

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -50,13 +50,42 @@ class Estudiante(base):
         return f"Estudiante: {self.name}, edad {self.age}, grado {self.grade}, tutor {self.tutor.name}"
 
 
+
 def create_schema():
     # Borrar todos las tablas existentes en la base de datos
     # Esta linea puede comentarse sino se eliminar los datos
     base.metadata.drop_all(engine)
-
     # Crear las tablas
     base.metadata.create_all(engine)
+
+
+
+def insert_tutor(name):
+    # Crear la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Crear un nuevo tutor
+    nuevo_tutor = Tutor(name=name)
+
+    # Agregar el nuevo tutor a la DB
+    session.add(nuevo_tutor)
+    session.commit()
+   
+
+
+def insert_estudiante(name, age, grade, tutor_id):
+    # Crear la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Crear un nuevo estudiante
+    nuevo_estudiante = Estudiante(name=name, age=age, grade=grade, tutor_id=tutor_id)
+   
+    # Agregar el nuevo estudiante a la DB
+    session.add(nuevo_estudiante)
+    session.commit()
+    
 
 
 def fill():
@@ -65,7 +94,11 @@ def fill():
     # Cada tutor tiene los campos:
     # id --> este campo es auto incremental por lo que no deberá completarlo
     # name --> El nombre del tutor (puede ser solo nombre sin apellido)
-
+    tutores =["Javier", "Claudio", "Marta"]
+   
+    for x in range(len(tutores)):
+        insert_tutor(tutores[x])
+    
     # Llenar la tabla de la secundaria con al menos 5 estudiantes
     # Cada estudiante tiene los posibles campos:
     # id --> este campo es auto incremental por lo que no deberá completarlo
@@ -73,17 +106,38 @@ def fill():
     # age --> cuantos años tiene el estudiante
     # grade --> en que año de la secundaria se encuentra (1-6)
     # tutor --> el tutor de ese estudiante (el objeto creado antes)
+    estudiantes = [ ("Pablo", 12, 1, 1),
+                    ("Veronica", 13, 2, 2),
+                    ("Pedro", 14, 3, 3),
+                    ("Shara", 15, 4, 1),
+                    ("Juan", 16, 5, 2),
+                    ("Julian", 17, 6, 3)]
 
+    for x in range(len(estudiantes)):
+            insert_estudiante(estudiantes[x][0] ,estudiantes[x][1], estudiantes[x][2], estudiantes[x][3])
+  
     # No olvidarse que antes de poder crear un estudiante debe haberse
     # primero creado el tutor.
+
 
 
 def fetch():
     print('Comprovemos su contenido, ¿qué hay en la tabla?')
     # Crear una query para imprimir en pantalla
     # todos los objetos creaods de la tabla estudiante.
+   
+    # Crear la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Buscar todos los estudiantes
+    query = session.query(Estudiante)
+
     # Imprimir en pantalla cada objeto que traiga la query
     # Realizar un bucle para imprimir de una fila a la vez
+    for estudiante in query:
+        print(estudiante)
+
 
 
 def search_by_tutor(tutor):
@@ -92,23 +146,53 @@ def search_by_tutor(tutor):
     # Crear una query para imprimir en pantalla
     # aquellos estudiantes que tengan asignado dicho tutor.
 
+    # Crear la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(Estudiante).join(Estudiante.tutor).filter(Tutor.name == tutor)
+        
+    print("Estudiantes del tutor", tutor)
+    for x in query:
+            print(x.name)
+
     # Para poder realizar esta query debe usar join, ya que
     # deberá crear la query para la tabla estudiante pero
     # buscar por la propiedad de tutor.name
 
 
-def modify(id, name):
+
+def modify(id, nuevo_tutor):
     print('Modificando la tabla')
-    # Deberá actualizar el tutor de un estudiante, cambiarlo para eso debe
-    # 1) buscar con una query el tutor por "tutor.name" usando name
+    # Deberá actualizar el tutor de un estudiante, cambiarlo para eso debera:
+    
+    # Crear la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    # 1) buscar con una query el tutor por "tutor.name" usando nuevo_tutor
     # pasado como parámetro y obtener el objeto del tutor
+    query = session.query(Tutor).filter(Tutor.name == nuevo_tutor)
+    tutor_act = query.first()
+    
     # 2) buscar con una query el estudiante por "estudiante.id" usando
     # el id pasado como parámetro
+    query = session.query(Estudiante).filter(Estudiante.id == id)
+    estudiante_act = query.first()
+    
     # 3) actualizar el objeto de tutor del estudiante con el obtenido
     # en el punto 1 y actualizar la base de datos
+    estudiante_act.tutor = tutor_act
 
+    # Aunque la persona ya existe, como el id coincide
+    # se actualiza sin generar una nueva entrada en la DB
+    session.add(estudiante_act)
+    session.commit()
+
+    print("Estudiante actualizado Id:", id)
     # TIP: En clase se hizo lo mismo para las nacionalidades con
-    # en la función update_persona_nationality
+    # la función update_persona_nationality
+
 
 
 def count_grade(grade):
@@ -117,22 +201,31 @@ def count_grade(grade):
     # se encuentran cursando el grado "grade" pasado como parámetro
     # Imprimir en pantalla el resultado
 
+    # Crear la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(Estudiante).filter(Estudiante.grade == grade).count()
+    print("Estudiantes en grado:", grade, "encontrados:", result)
     # TIP: En clase se hizo lo mismo para las nacionalidades con
     # en la función count_persona
+
 
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     create_schema()   # create and reset database (DB)
-    # fill()
-    # fetch()
 
-    tutor = 'nombre_tutor'
-    # search_by_tutor(tutor)
+    fill()
 
-    nuevo_tutor = 'nombre_tutor'
+    fetch()
+
+    tutor = "Claudio"
+    search_by_tutor(tutor)
+    
+    nuevo_tutor = "Marta"
     id = 2
-    # modify(id, nuevo_tutor)
+    modify(id, nuevo_tutor)
 
     grade = 2
-    # count_grade(grade)
+    count_grade(grade)
